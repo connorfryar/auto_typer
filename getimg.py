a5b5ffe1c362636cc5b7dac847fbe42e84dd2370
connorfryar <connorfryar@gmail.com>
2022-06-14 14:58:35 -0500

    Screen shot now captures short length text. HARD CODED

--- getimg.py
+++ getimg.py
@@ -12,7 +12,9 @@ def lanchBrowser():
     
     
     driver.get("https://www.how-to-type.com/typing-practice/quote/")
+    time.sleep(4)
     return driver
+
 driver = lanchBrowser()
 
 def getScreenshot():
@@ -22,7 +24,7 @@ def getScreenshot():
     shows final product
     '''
     # navigation and image capture
-    element = driver.find_element_by_class_name("quote")
+    element = driver.find_element_by_id("content")
     location = element.location
     size = element.size
     driver.save_screenshot('ss.png')
@@ -33,19 +35,20 @@ def getScreenshot():
     width = location['x']+size['width']
     height = location['y']+size['height']
     im = Image.open('ss.png')
-    im = im.crop((int(x), int(y), int(width), int(height)))
-    im.save('element.png')    
+    im = im.crop((x+650, y+800, width+1070, height+100))
+    im.save('element.png')   
 
     # shows results
     screenshot = Image.open('ss.png')
     screenshot.show()
     im.show()
+    print(element, location, size, x, y, width, height)
 
 getScreenshot()
 
 
 def closeBrowser():
-    time.sleep(10)
+    time.sleep(3)
     driver.close()
     return driver
 
